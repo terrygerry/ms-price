2de7f25bd540ceb5a850b9a0368a9ff9f2d2a347
gerrysalazar <terrygerry9@gmail.com>
2025-05-16 01:01:27 -0500

    feat: Create base project

---
+++ src/main/java/com/technical_test/ms_price/MsPriceApplication.java
@@ -0,0 +1,13 @@
+package com.technical_test.ms_price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technical_test/ms_price/MsPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical_test.ms_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
