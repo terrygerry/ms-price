8d512a39af241ccddf3cc4b5ea630374f67a03ff
gerrysalazar <terrygerry9@gmail.com>
2025-05-18 05:47:11 -0500

    test: Add integration tests for infrastructure layer

---
+++ src/test/java/com/technical_test/ms_price/infrastructure/controller/PriceControllerTest.java
@@ -0,0 +1,65 @@
+package com.technical_test.ms_price.infrastructure.controller;
+
+import com.technical_test.ms_price.application.dto.request.ProductPriceQueryDTO;
+import com.technical_test.ms_price.application.dto.response.ProductPriceDTO;
+import com.technical_test.ms_price.application.port.inbound.GetProductPriceUseCase;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebFluxTest(PriceController.class)
+public class PriceControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockitoBean
+    private GetProductPriceUseCase getProductPriceUseCase;
+
+    @Test
+    public void getPriceListByParameters_ValidInput_ReturnsOk() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
+        LocalDateTime dateTime = LocalDateTime.parse("2020-06-14-10.00.00",formatter);
+
+        ProductPriceQueryDTO queryDTO = new ProductPriceQueryDTO(35455,1, dateTime);
+
+        ProductPriceDTO expectedResponse = new ProductPriceDTO(
+                35455,
+                1,
+                1,
+                new BigDecimal("25.50"),
+                LocalDateTime.parse("2020-06-14T00:00:00"),
+                LocalDateTime.parse("2020-12-31T23:59:59"));
+
+        Mockito.when(getProductPriceUseCase.getProductPriceByCriteria(Mockito.any(ProductPriceQueryDTO.class)))
+                .thenReturn(Mono.just(expectedResponse));
+
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/prices")
+                        .queryParam("productId", queryDTO.productId())
+                        .queryParam("brandId", queryDTO.brandId())
+                        .queryParam("dateFrom", queryDTO.dateFrom())
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(ProductPriceDTO.class)
+                .isEqualTo(expectedResponse);
+
+        Mockito.verify(getProductPriceUseCase, Mockito.times(1))
+                .getProductPriceByCriteria(Mockito.any(ProductPriceQueryDTO.class));
+    }
+
+}
---
+++ src/test/java/com/technical_test/ms_price/infrastructure/repository/ReactivePriceRepositoryTest.java
@@ -0,0 +1,78 @@
+package com.technical_test.ms_price.infrastructure.repository;
+
+import com.technical_test.ms_price.domain.model.Price;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@DataR2dbcTest
+public class ReactivePriceRepositoryTest {
+
+    @Autowired
+    private ReactivePriceRepository reactivePriceRepository;
+
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    @Test
+    public void findApplicablePricesByProductIdAndBrandIdAndDate_returnsCorrectPrices() {
+
+        insertTestData(1,
+                1,
+                LocalDateTime.parse("2020-06-14T00:00:00"),
+                LocalDateTime.parse("2020-12-31T23:59:59"),
+                1,
+                35455,
+                0,
+                new BigDecimal("35.50"),
+                "EUR");
+
+        insertTestData(2,
+                1,
+                LocalDateTime.parse("2020-06-15T00:00:00"),
+                LocalDateTime.parse("2020-06-15T11:00:00"),
+                3,
+                35455,
+                1,
+                new BigDecimal("30.50"),
+                "EUR");
+
+        insertTestData(3,
+                1,
+                LocalDateTime.parse("2020-06-15T16:00:00"),
+                LocalDateTime.parse("2020-12-31T23:59:59"),
+                4,
+                35455,
+                1,
+                new BigDecimal("38.95"),
+                "EUR");
+
+        Flux<Price> prices = reactivePriceRepository.findApplicablePricesByProductIdAndBrandIdAndDate(35455, 1, LocalDateTime.parse("2020-06-15T10:00:00"));
+
+        StepVerifier.create(prices)
+                .expectNextMatches(price -> price.getPrice().equals(new BigDecimal("35.50")) || price.getPrice().equals(new BigDecimal("30.50")))
+                .expectNextMatches(price -> price.getPrice().equals(new BigDecimal("35.50")) || price.getPrice().equals(new BigDecimal("30.50")))
+                .verifyComplete();
+    }
+
+    private void insertTestData(Integer id, Integer brandId, LocalDateTime startDate, LocalDateTime endDate, Integer priceList, Integer productId, Integer priority, BigDecimal priceValue, String currency) {
+        databaseClient.sql("INSERT INTO prices (id, brand_id, start_date, end_date, price_list, product_id, priority, price, currency) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)")
+                .bind(0, id)
+                .bind(1, brandId)
+                .bind(2, startDate)
+                .bind(3, endDate)
+                .bind(4, priceList)
+                .bind(5, productId)
+                .bind(6, priority)
+                .bind(7, priceValue)
+                .bind(8, currency)
+                .fetch().rowsUpdated().block();
+    }
+
+}
