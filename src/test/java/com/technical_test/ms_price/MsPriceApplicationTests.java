dd5299123e13d1b9d02ad64b78eef2f12f790f0f
gerrysalazar <terrygerry9@gmail.com>
2025-05-19 05:32:12 -0500

    fix: Resolve SonarLint warning - Implement assertion in empty test

--- src/test/java/com/technical_test/ms_price/MsPriceApplicationTests.java
+++ src/test/java/com/technical_test/ms_price/MsPriceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.technical_test.ms_price;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsPriceApplicationTests {
+
+	@Autowired
+	private MsPriceApplication applicationContext;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(applicationContext);
 	}
 
 }
