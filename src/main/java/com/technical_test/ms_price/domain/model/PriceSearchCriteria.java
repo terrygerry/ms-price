1a0d603afa6faec292f761e29889a9f2fdcd03c3
gerrysalazar <terrygerry9@gmail.com>
2025-05-27 03:59:55 -0500

    refactor: Decouple domain classes from framework-specific dependencies

--- src/main/java/com/technical_test/ms_price/domain/model/PriceSearchCriteria.java
+++ src/main/java/com/technical_test/ms_price/domain/model/PriceSearchCriteria.java
@@ -1,16 +1,33 @@
 package com.technical_test.ms_price.domain.model;
 
-import lombok.*;
-
 import java.time.LocalDateTime;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class PriceSearchCriteria {
     private Integer productId;
     private Integer brandId;
     private LocalDateTime dateFrom;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public LocalDateTime getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(LocalDateTime dateFrom) {
+        this.dateFrom = dateFrom;
+    }
 }
---
+++ src/main/java/com/technical_test/ms_price/domain/model/ProductPrice.java
@@ -0,0 +1,89 @@
+package com.technical_test.ms_price.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class ProductPrice {
+
+    private Integer id;
+    private Integer brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Integer productId;
+    private int priority;
+    private BigDecimal price;
+    private String currency;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(Integer priceList) {
+        this.priceList = priceList;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
