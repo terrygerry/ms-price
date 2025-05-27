c552c611cc667a35da60cb058f390505ba70cf8d
gerrysalazar <terrygerry9@gmail.com>
2025-05-27 03:58:07 -0500

    feat: Optimize persistence layer for improved data operations

--- src/main/java/com/technical_test/ms_price/infrastructure/repository/ReactivePriceRepository.java
+++ src/main/java/com/technical_test/ms_price/infrastructure/repository/ReactivePriceRepository.java
@@ -1,18 +1,21 @@
 package com.technical_test.ms_price.infrastructure.repository;
 
-import com.technical_test.ms_price.domain.model.PriceEntity;
+import com.technical_test.ms_price.infrastructure.repository.data.ProductPriceData;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.time.LocalDateTime;
 
-public interface ReactivePriceRepository extends R2dbcRepository<PriceEntity, Integer> {
+public interface ReactivePriceRepository extends R2dbcRepository<ProductPriceData, Integer> {
 
     @Query("SELECT * FROM prices " +
             "WHERE product_id = :productId " +
             "AND brand_id = :brandId " +
             "AND start_date <= :dateFrom " +
-            "AND end_date >= :dateFrom ")
-    Flux<PriceEntity> findApplicablePricesByProductIdAndBrandIdAndDate(Integer productId, Integer brandId, LocalDateTime dateFrom);
+            "AND end_date >= :dateFrom " +
+            "ORDER BY priority DESC " +
+            "LIMIT 1")
+    Mono<ProductPriceData> findApplicablePricesByProductIdAndBrandIdAndDate(Integer productId, Integer brandId, LocalDateTime dateFrom);
 
 }
